6a354bf4ee5cb57965037118bc64b39685d88699
Sean Twie03 <sean.twie03@gmail.com>
2026-05-10 07:43:21 -0500

    2026-05-10T07:43-05:00

--- dotfiles/nvim/lua/config/ui.lua
+++ dotfiles/nvim/lua/config/ui.lua
@@ -2,10 +2,6 @@
 --------------------------------------------------------------------------------
 vim.opt.winborder = 'rounded'
 vim.opt.termguicolors = true
-vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme modus]]
 --vim.cmd[[colorscheme tokyonight]]
-
--- Modus Operandi colorscheme from Emacs Prot. I copy/pasted this file from:
--- https://gitlab.com/protesilaos/dotfiles/-/blob/master/vim/.vim/colors/modus_operandi.vim
---vim.cmd.colorscheme("modus_operandi")
 
