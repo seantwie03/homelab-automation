0d61f256c8721562a56c55aed66c687220fad625
Sean Twie03 <sean.twie03@gmail.com>
2026-06-02 19:39:08 -0500

    2026-06-02T19:39-05:00

--- dotfiles/waybar/waybar-launch.sh
+++ dotfiles/waybar/waybar-launch.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 case "${XDG_CURRENT_DESKTOP,,}" in
     niri)
+        host_config="$HOME/.config/waybar/niri.$(hostname --short).config.jsonc"
+        host_style="$HOME/.config/waybar/niri.$(hostname --short).style.css"
+        if [ -f "$host_config" ]; then
+            if [ -f "$host_style" ]; then
+                exec waybar --config "$host_config" --style "$host_style"
+            fi
+            exec waybar --config "$host_config"
+        fi
         exec waybar --config "$HOME/.config/waybar/niri.config.jsonc"
         ;;
     hyprland)
