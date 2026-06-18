a8d4fe65af262b22f38cf0feea90768c495a4c52
Sean Twie03 <sean.twie03@gmail.com>
2026-06-18 07:54:33 -0500

    2026-06-18T07:54-05:00

--- dotfiles/nvim/lua/plugins/codediff.lua
+++ dotfiles/nvim/lua/plugins/codediff.lua
@@ -4,7 +4,7 @@ return {
     opts = {},
     keys = {
         {
-            "<leader>gd",
+            "<leader>gr",
             "<Cmd>CodeDiff<CR>",
             desc = "Git diff",
         },
--- dotfiles/nvim/lua/plugins/diffview.lua
+++ dotfiles/nvim/lua/plugins/diffview.lua
@@ -1,11 +1,33 @@
 return {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    "dlyongemallo/diffview-plus.nvim",
+    cmd = {
+        "DiffviewOpen",
+        "DiffviewToggle",
+        "DiffviewFileHistory",
+        "DiffviewDiffFiles",
+        "DiffviewLog",
+    },
+    opts = {
+        enhanced_diff_hl = true,
+        diffopt = {
+            algorithm = "histogram",
+            linematch = 60,
+        },
+        file_panel = {
+            listing_style = "tree",
+            win_config = { position = "left", width = "auto" },
+        },
+    },
     keys = {
+        {
+            "<leader>gd",
+            "<Cmd>DiffviewOpen<CR>",
+            desc = "DiffviewOpen"
+        },
         {
             "<leader>gD",
             "<Cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<CR>",
-            desc = "Review MR (diffview)"
+            desc = "Review MR (checkout MR branch first)"
         }
     }
 }
