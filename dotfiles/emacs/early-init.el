c914fbb8a394cc614498d8caf2c68ea25170a568
Sean Twie03 <sean.twie03@mail.com>
2026-07-02 18:01:56 -0500

    2026-07-02T18:01-05:00

--- dotfiles/emacs/early-init.el
+++ dotfiles/emacs/early-init.el
@@ -19,7 +19,6 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'tooltip-mode) (tooltip-mode -1))
-(if (fboundp 'fringe-mode) (fringe-mode -1))
 
 (setq default-frame-alist '((fullscreen . maximized)
 
